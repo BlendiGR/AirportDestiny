0a37a4176395dd54cd837195500b281693e5284a
JarkkoKarki <karkijarkko@gmail.com>
2024-02-27 10:53:53 +0200

    sqlll selitykset lisätty

--- Airportdestiny/resources.py
+++ Airportdestiny/resources.py
@@ -2,7 +2,7 @@ import mysql.connector
 import random
 
 
-
+# Yhteys funktio tekee yhteyden tietokantaan.
 def yhteys():
     connection = mysql.connector.connect(
         host="127.0.0.1",
@@ -14,9 +14,10 @@ def yhteys():
     )
     return connection
 
-
+# satunnaiset maat funktio valitsee tietokannasta 20 EU:n suurta lentokenttää ja tekee niistä listan.
 def satunnaiset_maat(sql_yhteys):
 
+# Valitaan tietokannasta kaikki EU:n suuret lentokentät
     cursor = sql_yhteys.cursor()
     sql = ("""select airport.name from airport, country
     where airport.iso_country=country.iso_country 
@@ -26,17 +27,20 @@ def satunnaiset_maat(sql_yhteys):
     result = cursor.fetchall()
 
 
-    # random.sample varmistaa, että luvut eivät toistu
+    # tehdään tyhjä lista valituille maille
     pelilauta = []
-
+# random.sample varmistaa, että satunnaisesti valitut luvut eivät toistu
     satunnainen_luku = random.sample(range(len(result)), 10)
+# lisätään jokaista lukua vastaava maa tietokannasta listaan
     for num in satunnainen_luku:
         maa = result[num][0]
         pelilauta.append(maa)
 
+# printataan jokainen maa
     for n in pelilauta:
         print(n)
     return pelilauta
 
+#kutsutaan funktiot
 sql_yhteys = yhteys()
 pelilauta = satunnaiset_maat(sql_yhteys)
