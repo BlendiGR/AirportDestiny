ebf3f0925302df05d36e746e2afec6e36add22c6
BlendinAMKura <blendi.gr@hotmail.fi>
2024-02-27 13:12:37 +0200

    lentokoneet ja pohja intro valmiiksi.

---
+++ Airportdestiny/lentokoneet.py
@@ -0,0 +1,44 @@
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+RESET = '\033[0m'
+
+
+lentokone_esittely = (f'''
+{BLUE}1. Sähkölentokone Ultra{RESET}
+
+{GREEN}CO2 Päästöt: 0.25 kg/km
+Hinta: 6000€{RESET}
+Kuvaus: Huipputeknologian nollapäästöinen lentokone, joka tarjoaa vertaansa vailla olevaa tehokkuutta ja ympäristöystävällisyyttä premium-hinnalla
+ ''',f''' 
+{BLUE}Hybridi Helikopteri{RESET}
+
+{GREEN}CO2 Päästöt: 0.30 kg/km
+Hinta: 5000€{RESET}
+Kuvaus: Sähkömoottoreiden ja perinteisen polttoaineen yhdistelmä tarjoaa tasapainon vähentyneiden päästöjen ja luotettavan suorituskyvyn välillä.
+ ''', f'''
+{BLUE}Sähköliitokone Pro{RESET}
+
+{GREEN}CO2 Päästöt: 0.35 kg/km
+Hinta: 4000€{RESET}
+Kuvaus: Edistyneellä akkuteknologialla varustettu sähköliitokone on suunniteltu tehokkuutta ja mahdollisimman pientä ympäristövaikutusta silmällä pitäen, ihanteellinen keskitason budjeteille.
+ ''',f'''
+{BLUE}Biodiesel Jet{RESET}
+
+{GREEN}CO2 Päästöt: 0.40 kg/km
+Hinta: 3000€{RESET}
+Kuvaus: Kestävillä biopolttoaineilla toimiva jet asettaa uuden standardin ympäristöystävälliselle nopealle matkustamiselle saavutettavaan hintaan.
+ ''',f'''
+{BLUE}Sähköhelikopteri Mini{RESET}
+
+{GREEN}CO2 Päästöt: 0.45 kg/km
+Hinta: 2000€{RESET}
+Kuvaus: Kompakti ja monipuolinen sähköhelikopteri, joka tarjoaa pienemmät päästöt lyhyen matkan tehtäviin ja budjettitietoisille pelaajille.
+ ''',f'''
+{BLUE} Aurinkovoima Raketti{RESET}
+ 
+{GREEN}CO2 Päästöt: 0.50 kg/km
+Hinta: 1000€{RESET}
+Kuvaus: Innovaatiivinen konsepti, joka yhdistää aurinkoenergian ja raketin teknologian, tarjoten ainutlaatuisen, mutta vähemmän tehokkaan matkustustavan alhaisimmalla hinnalla.
+ ''')
--- Airportdestiny/pohja.py
+++ Airportdestiny/pohja.py
@@ -1,5 +1,6 @@
 import asetukset
-# import resources
+from resources import pelilauta
+from lentokoneet import lentokone_esittely
 RED = '\033[31m'
 GREEN = '\033[32m'
 YELLOW = '\033[33m'
@@ -7,22 +8,16 @@ BLUE = '\033[34m'
 RESET = '\033[0m'
 
 
-example = r'''
+example = BLUE + r'''
      _    _                       _     ____            _   _             
     / \  (_)_ __ _ __   ___  _ __| |_  |  _ \  ___  ___| |_(_)_ __  _   _ 
    / _ \ | | '__| '_ \ / _ \| '__| __| | | | |/ _ \/ __| __| | '_ \| | | |
   / ___ \| | |  | |_) | (_) | |  | |_  | |_| |  __/\__ \ |_| | | | | |_| |
  /_/   \_\_|_|  | .__/ \___/|_|   \__| |____/ \___||___/\__|_|_| |_|\__, |
                 |_|                                                 |___/ 
-'''
+''' + RESET
 print(example)
-
-RED = '\033[31m'
-GREEN = '\033[32m'
-YELLOW = '\033[33m'
-BLUE = '\033[34m'
-RESET = '\033[0m'
-
+input(f"{BLUE} Tervetuloa Airport Destiny peliin! {RESET} Peliä voi pelata 1 - 4 henkilöä. Peli arpoaa satunnaisesti 10 lentoaseman reitin. \n Vähäisimillä päästöillä kohdemaahan saapunut pelaaja voittaa pelin. {GREEN} (Paina ENTER jatkaaksesi){RESET}")
 
 def määrä_määrittely():
     # PELAAJAN MÄÄRÄ -----------------------------------------------------
@@ -40,14 +35,38 @@ def määrä_määrittely():
         except ValueError:
             print("Virheellinen syöttö")
 
+
 def pelaaja_nimi(määrä):
     pelaajat = []
+    nimet = set()
     for i in range(1, määrä + 1):
-        name = input(f" Mikä on pelaajan {i} nimi: ")
-        pelaajat.append([name, 0])
+        while True:
+            name = input(f" Mikä on pelaajan {i} nimi: ").strip()
+            if name and name not in nimet:
+                pelaajat.append([name, 0])
+                nimet.add(name)
+                break
+            else:
+                if not name:
+                    print("Nimi ei voi olla tyhjä. Yritä uudelleen.")
+                else:
+                    print("Nimi on jo käytössä. Anna eri nimi.")
     return pelaajat
+
 
 määrä = määrä_määrittely()
 
-pelaaja_nimi(määrä)
+pelaajat = pelaaja_nimi(määrä)
+
+
+print("Reitti on seuraavanlainen : ")
+
+for i, lentoasema in enumerate(pelilauta, start=1):
+    print(f"{i} -{GREEN} {lentoasema}{RESET}")
+
+input(f"Aloitus summa on 1000€ ja tavoitteena on lentää {pelilauta[-1]}:iin mahdolisimman vähäisillä päästöillä. {GREEN} Paina ENTER jatkaaksesi{RESET}")
+input(f"1 nopan silmäluku vastaa 1000€ ja kalliimat lentokoneet päästävät vähiten. {GREEN} Paina ENTER nähdääksesi vaihtoehdot.{RESET}")
+for lentokone in lentokone_esittely:
+    print(lentokone)
+
 
