d2fe312bbc9fd65960633836ff21cfadcc3392e8
BlendinAMKura <blendi.gr@hotmail.fi>
2024-02-27 18:10:23 +0200

    lisää paskaa

--- Airportdestiny/pohja.py
+++ Airportdestiny/pohja.py
@@ -1,13 +1,15 @@
 import asetukset
 from resources import pelilauta
 from lentokoneet import lentokone_esittely
+import random
+#VÄRIKOODIT ----------------------------------------------------------------------
 RED = '\033[31m'
 GREEN = '\033[32m'
 YELLOW = '\033[33m'
 BLUE = '\033[34m'
 RESET = '\033[0m'
 
-
+#INTRO-----------------------------------------------------------------------------
 example = BLUE + r'''
      _    _                       _     ____            _   _             
     / \  (_)_ __ _ __   ___  _ __| |_  |  _ \  ___  ___| |_(_)_ __  _   _ 
@@ -19,8 +21,8 @@ example = BLUE + r'''
 print(example)
 input(f"{BLUE} Tervetuloa Airport Destiny peliin! {RESET} Peliä voi pelata 1 - 4 henkilöä. Peli arpoaa satunnaisesti 10 lentoaseman reitin. \n Vähäisimillä päästöillä kohdemaahan saapunut pelaaja voittaa pelin. {GREEN} (Paina ENTER jatkaaksesi){RESET}")
 
+# PELAAJAN MÄÄRÄ -----------------------------------------------------------------
 def määrä_määrittely():
-    # PELAAJAN MÄÄRÄ -----------------------------------------------------
     while True:
         try:
             määrä = int(input("Montako pelaajaa? (Max 4) : "))
@@ -35,7 +37,7 @@ def määrä_määrittely():
         except ValueError:
             print("Virheellinen syöttö")
 
-
+#PELAAJIEN NIMET ------------------------------------------------------------------------------
 def pelaaja_nimi(määrä):
     pelaajat = []
     nimet = set()
@@ -53,12 +55,12 @@ def pelaaja_nimi(määrä):
                     print("Nimi on jo käytössä. Anna eri nimi.")
     return pelaajat
 
-
 määrä = määrä_määrittely()
-
 pelaajat = pelaaja_nimi(määrä)
 
 
+
+#REITTIEN KERTOMINEN JA LENTOKONE VAIHTOEHDOT --------------------------------------------------------------
 print("Reitti on seuraavanlainen : ")
 
 for i, lentoasema in enumerate(pelilauta, start=1):
@@ -69,4 +71,11 @@ input(f"1 nopan silmäluku vastaa 1000€ ja kalliimat lentokoneet päästävät
 for lentokone in lentokone_esittely:
     print(lentokone)
 
+input(f"{GREEN}Paina ENTER jatkaaksesi{RESET}")
 
+for i in pelaajat:
+    pelaajat.append(pelilauta[0])
+
+input(f"Aloitamme pelin {GREEN}{pelilauta[0]}:issa {RESET} ja seuraavana pysäkkinä on {GREEN}{pelilauta[1]}{RESET}. {GREEN}(Paina ENTER jatkaaksesi){RESET}")
+
+print(pelaajat)
