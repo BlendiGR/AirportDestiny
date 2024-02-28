0b1ddb296a80a75cdf909f9a7d01682a6b63a8bd
joni.heikkila <joni.heikkila@metropolia.fi>
2024-02-28 11:30:21 +0200

    Noppapeli: älkää väluttäny sotkusta

--- Airportdestiny/NoppaPeli.py
+++ Airportdestiny/NoppaPeli.py
@@ -1,41 +1,6 @@
 import random
-
-def määrä_määrittely():
-    # PELAAJAN MÄÄRÄ -----------------------------------------------------
-    while True:
-        try:
-            määrä = int(input("Montako pelaajaa? (Max 4) : "))
-            if määrä <= 0 or määrä > 4:
-                print("Sallittu pelaajien määrä on 1 - 4.")
-
-            elif määrä > 0 and määrä <=4:
-                return min(määrä, 4)
-                break
-
-
-        except ValueError:
-            print("Virheellinen syöttö")
-
-def pelaaja_nimi(määrä):
-    pelaajat = []
-    for i in range(1, määrä + 1):
-        name = input(f" Mikä on pelaajan {i} nimi: ")
-        pelaajat.append([name, 0])
-    return pelaajat
-
-määrä = määrä_määrittely()
-
-pelaaja_nimi(määrä)
 def heittää_noppaa():
     return random.randint(1, 6)
-
-#def heittojen_tulostus(pelaajat):
-#    for pelaaja in pelaajat:
-#        input(f"Pelaaja {pelaaja[0]}, paina Enter heittääksesi noppaa...")
-#        tulos = heittää_noppaa()
-#        pelaaja[1] += tulos * 1000
-#        print(f"Pelaaja {pelaaja['nimi']} heitti {tulos}. Hänellä on nyt {pelaaja[1]} rahaa.")
-#    print("Kaikki pelaajat ovat heittäneet noppaa.")
 
 def heittojen_tulostus(pelaajat):
     for pelaaja in pelaajat:
@@ -45,5 +10,8 @@ def heittojen_tulostus(pelaajat):
         print(f"Pelaaja {pelaaja['nimi']} heitti {tulos}. Hänellä on nyt {pelaaja[1]} rahaa.")
     print("Kaikki pelaajat ovat heittäneet noppaa.")
 
+
+heittää_noppaa()
+heittojen_tulostus()
 
 
