161af3267000a20f8623955c892ecd9f3b8579f9
JarkkoKarki <karkijarkko@gmail.com>
2024-02-28 13:23:30 +0200

    sql matkanvälinen etäisyys

--- Airportdestiny/resources.py
+++ Airportdestiny/resources.py
@@ -1,7 +1,6 @@
 import mysql.connector
 import random
-
-
+from geopy.distance import geodesic
 
 def yhteys():
     connection = mysql.connector.connect(
@@ -37,6 +36,19 @@ def satunnaiset_maat(sql_yhteys):
     for n in pelilauta:
         return pelilauta
 
+
+def maiden_välinenpituus():
+    cursor = sql_yhteys.cursor()
+    sql = (f"SELECT latitude_deg, longitude_deg FROM airport WHERE name = '{pelilauta[pelaajat[0][1]]}'")
+    sql2 = (f"SELECT latitude_deg, longitude_deg FROM airport WHERE name = '{pelilauta[pelaajat[0][1]-1]}'")
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    cursor.execute(sql2)
+    result2 = cursor.fetchall()
+    matka = geodesic(result[0], result2[0]).kilometers
+    return matka
+pelaajat = [["matti", 0, 0, 0], ["pekka", 0, 0, 1], ["joni", 0, 0, 4]]
 sql_yhteys = yhteys()
 pelilauta = satunnaiset_maat(sql_yhteys)
-
+matka = maiden_välinenpituus()
+print(matka)
