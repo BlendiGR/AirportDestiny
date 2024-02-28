bedcf3b44278f72dfb547d8478189a5df3e6e5a1
joni.heikkila <joni.heikkila@metropolia.fi>
2024-02-28 11:47:33 +0200

    Noppapeli thoimiiiiiii jauuuu

--- Airportdestiny/NoppaPeli.py
+++ Airportdestiny/NoppaPeli.py
@@ -1,4 +1,5 @@
 import random
+
 def heittää_noppaa():
     return random.randint(1, 6)
 
@@ -6,11 +7,10 @@ def heittojen_tulostus(pelaajat):
     for pelaaja in pelaajat:
         input(f"Pelaaja {pelaaja[0]}, paina Enter heittääksesi noppaa...")
         tulos = heittää_noppaa()
-        pelaaja['rahat'] += tulos * 1000
-        print(f"Pelaaja {pelaaja['nimi']} heitti {tulos}. Hänellä on nyt {pelaaja[1]} rahaa.")
+        pelaaja[1] += tulos * 1000
+        print(f"Pelaaja {pelaaja[0]} heitti {tulos}. Hänellä on nyt {pelaaja[1]} rahaa.")
     print("Kaikki pelaajat ovat heittäneet noppaa.")
+    return pelaajat
 
-
-heittää_noppaa()
-
+heittojen_tulostus(pelaajat)
 
